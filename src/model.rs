03c96b7783dfb355c6f327ca68da77a803101821
AuracleTech <VirgilFabre@outlook.com>
2024-04-25 23:27:32 -0400

    scaffolding camera and model

--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,21 @@
 #[derive(Clone, Debug, Copy)]
 pub struct Vertex {
     pub pos: [f32; 4],
-    pub color: [f32; 4],
+    pub color: [f32; 4], // TODO remove
+}
+
+#[derive(Debug)]
+pub struct Mesh {
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
+}
+
+#[derive(Debug)]
+pub struct Model {
+    pub meshes: Vec<Mesh>,
+}
+
+#[derive(Debug)]
+pub struct Scene {
+    pub models: Vec<Model>,
 }
