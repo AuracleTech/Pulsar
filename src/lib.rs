dbc23f6324120ba1873d7b08a8df8975bd171406
AuracleTech <VirgilFabre@outlook.com>
2024-04-29 02:51:02 -0400

    create_instance

--- src/lib.rs
+++ src/lib.rs
@@ -227,44 +227,7 @@ impl Engine {
                 .unwrap();
             let entry = Entry::linked();
 
-            let app_name = ffi::CStr::from_bytes_with_nul_unchecked(APP_NAME.as_bytes());
-            let appinfo = vk::ApplicationInfo::default()
-                .application_name(app_name)
-                .application_version(0)
-                .engine_name(app_name)
-                .engine_version(0)
-                .api_version(vk::make_api_version(0, 1, 0, 0));
-            let mut extension_names =
-                ash_window::enumerate_required_extensions(window.display_handle()?.as_raw())
-                    .unwrap()
-                    .to_vec();
-            extension_names.push(debug_utils::NAME.as_ptr());
-            #[cfg(any(target_os = "macos", target_os = "ios"))]
-            {
-                extension_names.push(ash::khr::portability_enumeration::NAME.as_ptr());
-                // Enabling this extension is a requirement when using `VK_KHR_portability_subset`
-                extension_names.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
-            }
-            let layer_names = [ffi::CStr::from_bytes_with_nul_unchecked(
-                b"VK_LAYER_KHRONOS_validation\0",
-            )];
-            let layers_names_raw: Vec<*const c_char> = layer_names
-                .iter()
-                .map(|raw_name| raw_name.as_ptr())
-                .collect();
-            let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
-                vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
-            } else {
-                vk::InstanceCreateFlags::default()
-            };
-            let create_info = vk::InstanceCreateInfo::default()
-                .application_info(&appinfo)
-                .enabled_layer_names(&layers_names_raw)
-                .enabled_extension_names(&extension_names)
-                .flags(create_flags);
-            let instance: Instance = entry
-                .create_instance(&create_info, None)
-                .expect("Instance creation error");
+            let instance = Engine::create_instance(&entry, &window)?;
 
             let pdevices = instance
                 .enumerate_physical_devices()
@@ -927,6 +890,52 @@ impl Engine {
                 event_loop,
             ))
         }
+    }
+
+    unsafe fn create_instance(
+        entry: &Entry,
+        window: &winit::window::Window,
+    ) -> Result<Instance, Box<dyn Error>> {
+        let app_name = ffi::CStr::from_bytes_with_nul_unchecked(APP_NAME.as_bytes());
+        let appinfo = vk::ApplicationInfo::default()
+            .application_name(app_name)
+            .application_version(0)
+            .engine_name(app_name)
+            .engine_version(0)
+            .api_version(vk::make_api_version(0, 1, 0, 0));
+        let mut extension_names =
+            ash_window::enumerate_required_extensions(window.display_handle()?.as_raw())
+                .unwrap()
+                .to_vec();
+        extension_names.push(debug_utils::NAME.as_ptr());
+        #[cfg(any(target_os = "macos", target_os = "ios"))]
+        {
+            extension_names.push(ash::khr::portability_enumeration::NAME.as_ptr());
+            // Enabling this extension is a requirement when using `VK_KHR_portability_subset`
+            extension_names.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
+        }
+        let layer_names = [ffi::CStr::from_bytes_with_nul_unchecked(
+            b"VK_LAYER_KHRONOS_validation\0",
+        )];
+        let layers_names_raw: Vec<*const c_char> = layer_names
+            .iter()
+            .map(|raw_name| raw_name.as_ptr())
+            .collect();
+        let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+            vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+        } else {
+            vk::InstanceCreateFlags::default()
+        };
+        let create_info = vk::InstanceCreateInfo::default()
+            .application_info(&appinfo)
+            .enabled_layer_names(&layers_names_raw)
+            .enabled_extension_names(&extension_names)
+            .flags(create_flags);
+        let instance: Instance = entry
+            .create_instance(&create_info, None)
+            .expect("Instance creation error");
+
+        Ok(instance)
     }
 
     unsafe fn create_debug_utils_messenger(
